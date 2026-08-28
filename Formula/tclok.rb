1f7ae9b0c3ebe6b70805fb16ac201b7939d7486d
Matheus Seabra <matheusvieiracoelho@gmail.com>
2026-08-28 09:59:26 -0300

    Release tclok 0.1.1

--- Formula/tclok.rb
+++ Formula/tclok.rb
@@ -1,8 +1,8 @@
 class Tclok < Formula
   desc "Dependency-free, resize-responsive terminal clock"
   homepage "https://github.com/matheuseabra/tclok"
-  url "https://github.com/matheuseabra/tclok/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "ce6b8b22332e3bfbb49a1037d33330fd0613e34683ba79b1f6fb7d1383a4cb6f"
+  url "https://crates.io/api/v1/crates/tclok/0.1.1/download"
+  sha256 "3f61824697af5ebaedca8c66a7f1521a3e28e9c5786379f1c5878aa680e88173"
   license "MIT"
 
   depends_on "rust" => :build
